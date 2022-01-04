1343fce84eb157ce6893414f19d1acde5d20f268
fuym <fuym@yuntongxun.com>
2022-01-04 11:58:03 +0800

    [feature]任务名拼接日报

--- zengdaoLogImport.user.js
+++ zengdaoLogImport.user.js
@@ -3,56 +3,29 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://zendao.yuntongxun.com/pro/effort-calendar.html
 // @grant       none
-// @version     1.1
+// @version     1.0
 // @author      fuym
 // @description 2021/9/24 下午6:12:18
-// @grant    GM.getValue
-// @grant    GM.setValue
-// @grant        GM_setValue
-// @grant        GM_getValue
 // @require https://unpkg.com/dayjs@1.8.21/dayjs.min.js
 // @require https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.8/clipboard.min.js
 // ==/UserScript==
 
 var utils = {
-    createButton: (id) => {
+    createButton: () => {
         let button = document.createElement("button");
         button.style = 'background: #f1f1f1;border: 1px solid #ccc;padding: 5px 10px;border-radius: 4px;margin-right: 10px;';
-        button.id = id;
         return button;
     },
-    createInput: (id) => {
-        let input = document.createElement("input");
-        input.style = 'border: 1px solid #ccc;padding: 5px 10px;border-radius: 4px;margin-right: 10px;';
-        input.id = id;
-        return input;
-    },
-    createCopyToClipboardBtn: (parentDom, btnDom, innerText) => {
+    createCopyToClipboardBtn: (parentDom, btnDom, txt, innerText) => {
         let button = utils.createButton();
         button.id = btnDom.substring(1);
         button.innerText = innerText;
         $(parentDom).append(button)
-        let clipboard = new ClipboardJS(btnDom, {
+        new ClipboardJS(btnDom, {
             text: (trigger) => {
-                let txt = trigger.getAttribute('aria-label');
-                if (txt) {
-                    return txt;
-                } else {
-                    alert("日志为空")
-                    return;
-                }
+                console.log(txt)
+                return txt;
             }
-        });
-        clipboard.on('success', function (e) {
-            console.info('Action:', e.action);
-            console.info('Text:', e.text);
-
-            e.clearSelection();
-        });
-
-        clipboard.on('error', function (e) {
-            console.error('Action:', e.action);
-            console.error('Trigger:', e.trigger);
         });
     },
     getHtml: (url) => {
@@ -63,16 +36,6 @@ var utils = {
                 return res
             }
         })
-    },
-    getCookie: (cname) => {
-        var name = cname + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i].trim();
-            console.log(c)
-            if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-        }
-        return "";
     }
 }
 
@@ -81,98 +44,177 @@ let ImportLog = () => {
     let weekstart = dayjs().add(-1, 'day').startOf('week').add(1, 'day').format('YYYY-MM-DD');
     let weekend = dayjs().add(-1, 'day').endOf('week').add(1, 'day').format('YYYY-MM-DD')
 
-    var yearLogs, nickname;
+    let nameMap = new Map([["taskId", "工单号"], ["remainder", "剩余"], ["start", "开始时间"], ["end", "结束时间"], ["taskName", "任务名称"], ["title", "工作内容"], ["taskName", "对象"], ["progress", "进度"], ["nickname", "登记人"], ["realStartDate", "实际开始"], ["totalConsumed", "总计消耗"], ["planEndDate", "截止日期"], ["planStartDate", "预计开始"], ["planRemainder", "预计剩余"], ["taskType", "任务状态"]])
 
-    let initBtn = async () => {
-        let username = GM_getValue("username")
-        if (!username || username == "") {
-            // TODO 填完用户名自动刷新
-            let input = utils.createInput("usernameInput");
-            input.placeholder = "请输入username"
-            $("#mainMenu").append(input)
-            $('#usernameInput').bind('keypress', (event) => {
-                if (event.keyCode == "13") {
-                    GM_setValue("username", $('#usernameInput').val())
-                    alert('你输入的内容为：' + GM_getValue("username"));
-                    $('#usernameInput').remove()
-                }
-            });
-            return;
+    // 工单号	工单类型	状态	优先级	主题	负责人	开始日期	计划完成日期	l
+    let ExcleList = ["taskId", "需求", "taskType", "一般", "taskName", "nickname", "realStartDate", "planEndDate", "progress"]
+    
+    let getNameKey = (val) => {
+        for (var [key, value] of nameMap) {
+            if (value == val) {
+                return key
+            }
         }
-        let sdate = utils.createInput("sdate")
-        sdate.type = "date";
-        sdate.value = weekstart;
-        let edate = utils.createInput("edate")
-        edate.type = "date";
-        edate.value = weekend;
-        $("#mainMenu").append(sdate)
-        $("#mainMenu").append(edate)
-        $("#sdate").change(() => {
-            changeContent()
-        });
-        $("#edate").change(() => {
-            changeContent()
-        });
-        yearLogs = await getYearLogs()
-        if (ClipboardJS.isSupported()) {
-            utils.createCopyToClipboardBtn("#mainMenu", "#copyDurationButton", "复制日志")
-            utils.createCopyToClipboardBtn("#mainMenu", "#copyTodayButton", "复制本日日志")
-            utils.createCopyToClipboardBtn("#mainMenu", "#copyWeekButton", "复制一周日志")
-            initFixContent()
-            changeContent()
-        }
-    }
-    let getHTMLInfo = () => {
-        nickname = $("#userNav .user-name").text()
+        return val
     }
     let getYearLogs = () => {
         return new Promise((resolve) => {
-            let username = GM_getValue("username")
             $.ajax({
                 method: "get",
-                url: "https://zendao.yuntongxun.com/pro/effort-ajaxGetEfforts-" + username + "-2021.html",
+                url: "/pro/effort-ajaxGetEfforts-fuym-2021.html",
                 success: (res) => {
-                    getHTMLInfo();
                     resolve(JSON.parse(res))
                 }
             })
         })
     }
-    let initFixContent = () => {
-        if (!yearLogs || yearLogs.length < 1) return;
+    let getLogDetail = (url) => {
+        return new Promise((resolve) => {
+            $.ajax({
+                method: "get",
+                url: url,
+                success: (res) => {
+                    let detailHTML = $(res).find('#mainContent table tr')
+                    let detailInfo = {}
+                    for (let key in detailHTML) {
+                        if (detailHTML[key] && detailHTML[key].children && typeof (detailHTML[key].children) != 'function') {
+                            let name = getNameKey(detailHTML[key].children[0].innerText);
+                            let content = detailHTML[key].children[1].innerText
+                            content = content.replace(/[\r\n]/g, "").replace(/ /g, '');
+                            if (name == "remainder") {
+                                content = content.replace(/[^\d]*/g, '')
+                            }
+                            if (name == "taskName") {
+                                detailInfo.taskId = content.replace(/[^\d]*/g, '');
+                            }
+                            detailInfo[name] = content
+                        }
+                    }
+                    resolve(detailInfo)
+                    // var aHrefRegExp = /(?<=a href=').*?(?=')/gi;
+                    // let arr = res.toLowerCase().match(aHrefRegExp)
+                    // let url = arr[0]
+                }
+            })
+        })
+    }
+    let getTaskDetail = (id) => {
+        return new Promise((resolve) => {
+            $.ajax({
+                method: "get",
+                url: "/pro/task-view-" + id + ".html?onlybody=yes",
+                success: (res) => {
+                    let detailInfo = {}
+                    detailInfo.taskName = $(res).find('.page-title .text')[0].title;
 
-        let weekLogTxt = "";
-        let todayLogTxt = "";
+                    let detailHTML = [...$(res).find('#legendBasic table tr').toArray(), ...$(res).find('.detail-content table tr').toArray()]
+
+
+                    for (let key in detailHTML) {
+                        if (detailHTML[key] && detailHTML[key].children && typeof (detailHTML[key].children) != 'function') {
+                            let name = getNameKey(detailHTML[key].children[0].innerText);
+                            let content = detailHTML[key].children[1].innerText
+                            content = content.replace(/[\r\n]/g, "").replace(/ /g, '');
+                            if (name == "planRemainder" || name == "totalConsumed") {
+                                content = content.replace(/[^\d\.]*/g, '')
+                            }
+                            detailInfo[name] = content
+                        }
+                    }
+
+                    resolve(detailInfo)
+                }
+            })
+        })
+    }
+    let generateExcleContent = (taskInfo) => {
+        
+        let content = ""
+        for (let item of ExcleList) {
+            let c = nameMap.get(item)
+            if (c) {
+                content += (taskInfo[item] || taskInfo.log[0][item]);
+                if (item == "taskName") {
+                    for(let l of taskInfo.log){
+                        let title = l.title.substring(3)
+                        if (title !== "1") {
+                            content += "-" + title;
+                        }
+                    }
+                }
+            } else {
+                content += item;
+            }
+            content +=  "\t"
+        }
+
+
+        // content += "\t" + lastLog.nickname + "\t";
+        // if (taskInfo.progress == "100%") {
+        //     content += "已完成";
+        // } else {
+        //     content += "进行中";
+        // }
+        // content += "\t" + "王松林" + "\t" + "暂无" + "\t" + "暂无" + "\t" + taskInfo.planEndDate + "\t" + taskInfo.progress
+
+
+        return content
+    }
+    let initBtn = async () => {
+        let yearLogs = await getYearLogs();
+
+        let weekLog = {};
         for (let l = yearLogs.length - 1; l > 0; l--) {
             let log = yearLogs[l]
-            if (dayjs(log.end) < dayjs(weekstart) || dayjs(log.end) > dayjs(weekend)) {
+            if (dayjs(log.end) < dayjs(weekstart)) {
                 break;
             } else {
-                weekLogTxt += log.title.substring(3) + "\t" + nickname + "\t" + "已完成" + "\t" + "王松林" + "\t" + "暂无" + "\t" + "暂无" + "\t" + log.end + "\r\n";
+                let moreLogInfo = await getLogDetail(log.url)
+                for (let key in moreLogInfo) {
+                    if (!log[key] && moreLogInfo[key]) {
+                        log[key] = moreLogInfo[key]
+                    }
+                }
 
-                if (log.end == today) {
-                    todayLogTxt += log.title.substring(3) + "\t" + "低" + "\t" + "" + "\t" + "王松林" + "\t" + log.start + "\t" + log.start + "\t" + log.start + "\t" + log.start + "\t" + log.consumed + "\r\n";
+                if (!weekLog[log.taskId]) {
+                    weekLog[log.taskId] = {}
+                    weekLog[log.taskId].log = []
+                }
+                weekLog[log.taskId].log.push(log)
+            }
+        }
+
+
+        console.log(weekLog)
+        let todayLogTxt = "";
+        let weekLogTxt = "";
+
+        for (let key in weekLog) {
+            let taskInfo = weekLog[key]
+            let moreTaskInfo = await getTaskDetail(key)
+            for (let y in moreTaskInfo) {
+                if (!taskInfo[y] && moreTaskInfo[y]) {
+                    taskInfo[y] = moreTaskInfo[y]
                 }
             }
-        }
-        $("#copyTodayButton").attr('aria-label', todayLogTxt);
-        $("#copyWeekButton").attr('aria-label', weekLogTxt)
-    }
-    let changeContent = () => {
-        if (!yearLogs || yearLogs.length < 1) return;
 
-        let durationTxt = "";
-        let start = $('#sdate').val()
-        let end = $('#edate').val()
-        for (let l = yearLogs.length - 1; l > 0; l--) {
-            let log = yearLogs[l]
-            if (dayjs(log.end) < dayjs(start) || dayjs(log.end) > dayjs(end)) {
-                break;
-            } else {
-                durationTxt += log.title.substring(3) + "\t" + nickname + "\t" + "已完成" + "\t" + "王松林" + "\t" + "暂无" + "\t" + "暂无" + "\t" + log.end + "\r\n";
+            weekLogTxt += generateExcleContent(taskInfo) + "\r\n"
+            // if (lastLog.end == today) {
+            //     todayLogTxt += content
+            // }
+
+        }
+
+        console.log(weekLogTxt)
+        console.log(todayLogTxt)
+        if (ClipboardJS.isSupported()) {
+            if (todayLogTxt != "") {
+                utils.createCopyToClipboardBtn("#mainMenu", "#copyTodayButton", todayLogTxt, "复制本日日志")
+            }
+            if (weekLogTxt != "") {
+                utils.createCopyToClipboardBtn("#mainMenu", "#copyWeekButton", weekLogTxt, "复制一周日志")
             }
         }
-        $("#copyDurationButton").attr('aria-label', durationTxt)
     }
     return {
         initBtn
@@ -180,24 +222,3 @@ let ImportLog = () => {
 }
 
 ImportLog().initBtn()
-
-// function getLogDetail(url) {
-//     $.ajax({
-//         method: "get",
-//         url: url,
-//         success: (res) => {
-//             var aHrefRegExp = /(?<=a href=').*?(?=')/gi;
-//             let arr = res.toLowerCase().match(aHrefRegExp)
-//             console.log(arr)
-//             let url = arr[0]
-//             $.ajax({
-//                 method: "get",
-//                 url: url,
-//                 success: (res) => {
-//                     let taskInfo = $(res).find('.detail-content table tr')
-//                     console.log(taskInfo)
-//                 }
-//             })
-//         }
-//     })
-// }
